115f29a14f05a3c3901d9e47e6eda35f6f75f5c2
Vande Nikolovski <smekiboy@Vandes-MacBook-Pro.local>
2023-03-14 15:08:38 +0100

    update cors new

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.DATABASE_ACCESS, (error) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.options('*', cors())
+// app.options('*', cors())
 
 //  USE TO UPLOAD BATTERY ON THE MongoDB
 // app.use('/api/seed', batteryRouter)
